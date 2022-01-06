d0f8d9f3339f691936361a4d93c73750e81f8d92
Omar Orozco <omar.orozco@corponet.com.mx>
2022-01-06 16:17:16 -0600

    Primer commit agregando primer commit

--- gitKnowleage/Program.cs
+++ gitKnowleage/Program.cs
@@ -10,7 +10,7 @@ namespace gitKnowleage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo Git");
+            Console.WriteLine("Hola mundo Git primer commit");
         }
     }
 }
