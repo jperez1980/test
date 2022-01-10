030b8c40d4436894cd3cc659c21509eff1366a21
Omar Orozco <omar.orozco@corponet.com.mx>
2022-01-09 22:24:17 -0600

    update

--- gitKnowleage/Program.cs
+++ gitKnowleage/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using SAPbobsCOM;
 
 namespace gitKnowleage
 {
@@ -8,7 +7,7 @@ namespace gitKnowleage
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo Git primer commit");
-            Console.WriteLine("En la rama1");         
+            Console.WriteLine("En la rama1"); 
         }
     }
 }
